9a5a6408965a2b945c50da3dc95b0e1ffd9da405
Neel Parihar <neelparihar599@gmail.com>
2020-05-11 21:23:26 +0530

    Done first problem

--- 1. Arrays/Easy/1.cpp
+++ 1. Arrays/Easy/1.cpp
@@ -2,8 +2,19 @@
 
 using namespace std;
 
+// Create an Array of size 10 of integers. Take input from the user for these
+// 10 elements and print the entire array after that.
+
 int main()
 {
-    cout<<"hello world";
+    int arr[10];
+    for(int i=0;i<10;i++)
+    {
+        cin>>arr[i];
+    }
+    for(int i=0;i<10;i++)
+    {
+        cout<<arr[i]<<endl;
+    }
     return(0);
 }
